3347ad223b75105410d32e90f26a589e7745f5ea
Michal 'vorner' Vaner <vorner@vorner.cz>
2018-03-17 17:36:09 +0100

    Unify the group-by implementations
    The only difference between them is what kind of data structure is used
    as the inner type. This uses a private trait to unify both data
    structures to the same interface and keeps just one copy of the code.

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,24 @@ use regex::Regex;
 use std::collections::{BTreeSet, BTreeMap};
 use std::io::BufRead;
 
-fn groupby_unique(re: &Regex, group_id: Option<usize>) -> BTreeMap<String, BTreeSet<String>> {
-    let mut grouping: BTreeMap<String, BTreeSet<String>> = BTreeMap::new();
+trait Group: Default + IntoIterator<Item = String> {
+    fn add(&mut self, line: String);
+}
+
+impl Group for BTreeSet<String> {
+    fn add(&mut self, line: String) {
+        self.insert(line);
+    }
+}
+
+impl Group for Vec<String> {
+    fn add(&mut self, line: String) {
+        self.push(line);
+    }
+}
+
+fn groupby<G: Group>(re: &Regex, group_id: Option<usize>) -> BTreeMap<String, G> {
+    let mut grouping: BTreeMap<String, G> = BTreeMap::new();
 
     let stdin = std::io::stdin();
     for line in stdin.lock().lines() {
@@ -19,50 +35,28 @@ fn groupby_unique(re: &Regex, group_id: Option<usize>) -> BTreeMap<String, BTree
             None => "***NO-MATCH***",
         };
 
-        grouping.entry(capture.to_string()).or_insert(BTreeSet::new()).insert(line.clone());
+        grouping.entry(capture.to_string()).or_insert_with(Default::default).add(line.clone());
     }
 
     return grouping;
+}
+
+fn print_groupby<G: Group>(re: &Regex, group_id: Option<usize>) {
+    for (group, members) in groupby::<G>(&re, group_id) {
+        println!("{}", group);
+        for line in members {
+            println!("    {}", line);
+        }
+    }
 }
 
 fn print_groupby_unique(re: &Regex, group_id: Option<usize>) {
-    for (group, members) in groupby_unique(&re, group_id) {
-        println!("{}", group);
-        for line in members {
-            println!("    {}", line);
-        }
-    }
-}
-
-
-fn groupby_all(re: &Regex, group_id: Option<usize>) -> BTreeMap<String, Vec<String>> {
-    let mut grouping: BTreeMap<String, Vec<String>> = BTreeMap::new();
-
-    let stdin = std::io::stdin();
-    for line in stdin.lock().lines() {
-        let line = line.unwrap();
-
-
-        let capture = match re.captures(&line) {
-            Some(captures) => captures.get(group_id.unwrap_or(0)).unwrap().as_str(),
-            None => "***NO-MATCH***",
-        };
-
-        grouping.entry(capture.to_string()).or_insert(Vec::new()).push(line.clone());
-    }
-
-    return grouping;
+    print_groupby::<BTreeSet<String>>(re, group_id);
 }
 
 fn print_groupby_all(re: &Regex, group_id: Option<usize>) {
-    for (group, members) in groupby_all(&re, group_id) {
-        println!("{}", group);
-        for line in members {
-            println!("    {}", line);
-        }
-    }
+    print_groupby::<Vec<String>>(re, group_id);
 }
-
 
 fn main() {
     let matches = App::new("groupby (lostutils)")
